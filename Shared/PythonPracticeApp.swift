5c941325844796783386e7fd6f8a8953e475d418
Rob Sturgeon <rob.sturgeon@atomicmedia.co.uk>
2021-10-01 14:16:59 +0100

    Initial Commit

---
+++ Shared/PythonPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PythonPracticeApp.swift
+//  Shared
+//
+//  Created by Rob Sturgeon on 01/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct PythonPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
